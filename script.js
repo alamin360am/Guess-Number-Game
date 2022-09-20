6ec3b0dee34d483a58d5376ecae32d2667469fb2
Al Amin <86236918+alamin360am@users.noreply.github.com>
2022-09-20 14:07:41 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict'
+
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    
+    if (!guess) {
+        displayMessage('⛔️ Not a Number');
+    }
+    else if (guess === secretNumber) {
+        displayMessage('🎉 Correct Number!');
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    }
+    else if (guess !== secretNumber) {
+        displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+        if (score > 1) {
+                     score--;
+                     document.querySelector('.score').textContent = score;
+                 } else {
+                     displayMessage('💥 You lost the game!');
+                     document.querySelector('.score').textContent = 0;
+                 }
+    }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    displayMessage('Start guessing....');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#333';
+    document.querySelector('.number').style.width = '15rem';
+});
